aca0c16c564e2285df43b181be70f99b2ff79c6f
bbang7 <zxas1245@naver.com>
2024-08-28 12:07:38 +0900

    :wrench:fix: publish footer

---
+++ src/components/icons/footer/RightArrowIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function RightArrowIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g stroke="#929292" fill="none" fillRule="evenodd" strokeLinecap="square">
+        <path d="m9.5 7.5-3-3M9.5 7.5l-3 3" />
+      </g>
+    </svg>
+  )
+}
