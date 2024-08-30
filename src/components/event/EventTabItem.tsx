b158e3acefcfe467c9b08b68bef284d3da948053
jangdebug <jangdebug@naver.com>
2024-08-30 11:14:45 +0900

    :sparkles:feat: publishing event component
    testing filter component

---
+++ src/components/event/EventTabItem.tsx
@@ -0,0 +1,11 @@
+import { eventTabBadgeType } from '@/types/InitialDataTypes'
+import React from 'react'
+import { Button } from '../ui/button'
+
+export default function EventTabItem({
+  eventTabBageData,
+}: {
+  eventTabBageData: eventTabBadgeType
+}) {
+  return <Button>test</Button>
+}
---
+++ src/datas/dummy/event/TabDatas.ts
@@ -0,0 +1,31 @@
+import { eventTabBadgeType } from '@/types/InitialDataTypes'
+
+export const eventTabDatas: eventTabBadgeType[] = [
+  {
+    id: 1,
+    tabDescription: 'All',
+    tabName: '전체',
+    tabBadgeImageUrl: '',
+  },
+  {
+    id: 2,
+    tabDescription: '이벤트 혜택 아이콘',
+    tabName: '이벤트 혜택',
+    tabBadgeImageUrl:
+      'https://cdn-mo.sivillage.com/mo/assets/comm/image/04IconThemeMneuPlanVillage_b.svg',
+  },
+  {
+    id: 3,
+    tabDescription: '브랜드 기획전 아이콘',
+    tabName: '브랜드 기획전',
+    tabBadgeImageUrl:
+      'https://cdn-mo.sivillage.com/mo/assets/comm/image/04IconThemeMneuPlan_b.svg',
+  },
+  {
+    id: 4,
+    tabDescription: '제휴 혜택 아이콘',
+    tabName: '제휴 혜택',
+    tabBadgeImageUrl:
+      'https://cdn-mo.sivillage.com/mo/assets/comm/image/04IconThemeMneuBenefits_b.svg',
+  },
+]
