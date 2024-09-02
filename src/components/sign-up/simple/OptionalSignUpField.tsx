988b0c12223fd8558175096be8b21d1d77c42b08
bbang7 <zxas1245@naver.com>
2024-09-02 17:01:05 +0900

    :sparkles:feat: add birth select form

--- src/components/sign-up/simple/OptionalSignUpField.tsx
+++ src/components/sign-up/simple/OptionalSignUpField.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import BirthSelectForm from './optional-form-item/BirthSelectForm'
 import MarketingAgreement from './optional-form-item/MarketingAgreement'
 
 export default function OptionalSignUpField() {
@@ -7,11 +8,7 @@ export default function OptionalSignUpField() {
     <div className="mt-[37px] pb-[40px] px-[24px]">
       <p className="text-[18px] leading-[22px] text-[#131922] font-bold">선택항목</p>
       <>
-        <p className="mt-[33px] mb-[13px] text-[16px] leading-[22px] text-[#131922] font-bold">생년월일</p>
-
-        <div className="w-full h-[60px] bg-black">
-          <span className="text-white">Birth Select Part</span>
-        </div>
+        <BirthSelectForm />
       </>
       <>
         <p className="mt-[33px] mb-[13px] text-[16px] leading-[22px] text-[#131922] font-bold">주소</p>
