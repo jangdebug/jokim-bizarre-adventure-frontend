a9e85ff5e50ece8488ef389825a23ef32dbe3f8b
bbang7 <zxas1245@naver.com>
2024-09-03 17:59:20 +0900

    :sparkles:test: testing optional catch-all segments

---
+++ src/app/product/[[...slug]]/page.tsx
@@ -0,0 +1,19 @@
+export default function Product({ params }: { params: { slug: string[] } }) {
+  const slug = params.slug
+  const slugArray = Array.isArray(slug) ? slug : [slug]
+
+  const decodedSlugArray = slugArray.map((element) => decodeURIComponent(element))
+
+  const [mainCategory, middleCategory, sub1, sub2] = decodedSlugArray
+  console.log(mainCategory, middleCategory, sub1, sub2)
+
+  return (
+    <div className="h-[300vh]">
+      <h1>Product Page</h1>
+      <p>Main Category: {mainCategory || 'undf'}</p>
+      <p>Middle Category: {middleCategory || 'undf'}</p>
+      <p>Subcategory 1: {sub1 || 'undf'}</p>
+      <p>Subcategory 2: {sub2 || 'undf'}</p>
+    </div>
+  )
+}
--- src/components/layout/ProductCategoryInfoHeader.tsx
+++ src/components/layout/ProductCategoryInfoHeader.tsx
@@ -7,14 +7,14 @@
 import { useScrollEvent } from '@/hooks/UseScrollEvent'
 import Link from 'next/link'
 
-export default function ProductCategoryInfoHeader() {
-  const catgoryInfo: string[] = ['아우터', '여성의류']
+export default function ProductCategoryInfoHeader({ categoryInfo }: { categoryInfo?: string[] }) {
+  // const catgoryInfo: string[] = ['아우터', '여성의류']
   const isVisible = useScrollEvent()
 
   return (
     <header className={`h-[48px] w-full z-[101] ${isVisible ? 'sticky top-0' : ''}`}>
       <ul className="flex items-center pr-[16px] pl-[24px]  box-border bg-[#fff] ">
-        {catgoryInfo.map((category, index) => (
+        {categoryInfo?.map((category, index) => (
           <li
             key={index}
             className={`category-item relative text-[16px] leading-[19px] p-[16px_18px_13px_0] overflow-ellipsis text-left`}
