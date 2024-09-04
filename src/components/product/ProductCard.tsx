43050739bf9230846596824124b3aed7c85a8312
bbang7 <zxas1245@naver.com>
2024-09-04 12:23:06 +0900

    :wrench:fix: separate like button

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import SmallStarIcon from './SmallStarIcon'
-import EmptyHeartIcon from './EmptyHeartIcon'
-import FilledHeartIcon from './FilledHeartIcon'
+import SmallStarIcon from '../icons/product/SmallStarIcon'
+import EmptyHeartIcon from '../icons/product/EmptyHeartIcon'
+import FilledHeartIcon from '../icons/product/FilledHeartIcon'
+import LikeButton from '../ui/LikeButton'
 
-export default function ProductItem() {
+export default function ProductCard() {
   return (
     <>
-      <li className="w-full relative">
+      <li className="w-full relative mb-[36px]">
         <Image
           src={'https://image.sivillage.com/upload/C00001/s3/goods/org/369/240814027642369.jpg?RS=450&SP=1'}
           alt="productImg"
@@ -15,6 +16,8 @@ export default function ProductItem() {
           height={0}
           sizes="100vw"
           style={{ width: '100%', height: 'auto' }}
+          // loader={}
+          // loading=''
         />
         {/* 상품 상세로 link */}
         <Link href={'/product-detail'} className="px-[8px] py-[16px] flex flex-col">
@@ -26,17 +29,18 @@ export default function ProductItem() {
             <span className="text-[12px] leading-[18px] tracking-[-0.06px] font-bold text-[#d99c63]">50%</span>
             <span className="text-[12px] leading-[18px] tracking-[-0.06px]">1,000,000</span>
           </span>
+          {/* 리뷰가 있으면 별점이 나옴 */}
           <div className="flex items-center mt-[8px] text-[12px] leading-[14px]">
             <SmallStarIcon />
             <span className="ml-[4px]">5</span>
             <span className="ml-[2px] text-[#929292]">(9)</span>
           </div>
         </Link>
+        {/* 신상 여부 */}
         <p className="pl-[8px] mt-[-8px] text-[12px] leading-[18px] font-bold text-[#d99c63]">신상</p>
-        <button className="absolute top-[8px] right-[8px] z-10">
-          <EmptyHeartIcon />
-          {/* <FilledHeartIcon /> */}
-        </button>
+        <div className="absolute top-[8px] right-[8px] z-10">
+          <LikeButton />
+        </div>
       </li>
     </>
   )
--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,21 +1,17 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import SmallStarIcon from '../icons/product/SmallStarIcon'
-import EmptyHeartIcon from '../icons/product/EmptyHeartIcon'
-import FilledHeartIcon from '../icons/product/FilledHeartIcon'
-import ProductItem from '../icons/product/ProductItem'
+import ProductCard from './ProductCard'
 
 export default function ProductList() {
+  // 상품 리스트 데이터 fetch
   return (
     <>
       <ul className="grid grid-cols-2 w-full gap-x-2 px-[24px]">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </ul>
     </>
   )
