a8707b458c992ccfb81e32e5620687898bf8baed
bbang7 <zxas1245@naver.com>
2024-09-10 00:54:13 +0900

    :wrench:chore: handle exception

--- src/app/brand/favorite/page.tsx
+++ src/app/brand/favorite/page.tsx
@@ -8,8 +8,16 @@ export default function Favorite() {
     <main className="px-[24px] mb-[80px]">
       <div className="mb-[9px] h-[48px] border-b border-[#f0f0f0]">
         <FilledHeartIcon />
-        <ul>{favoriteBrands?.map((item) => <BrandListItem key={item.id} brand={item} />)}</ul>
       </div>
+      <ul>
+        {favoriteBrands.length > 0 ? (
+          favoriteBrands?.map((item) => <BrandListItem key={item.id} brand={item} />)
+        ) : (
+          <p className="py-[20px] px-[24px] flex items-center justify-center text-[#929292]">
+            브랜드 찜 목록이 비어있습니다.
+          </p>
+        )}
+      </ul>
     </main>
   )
 }
