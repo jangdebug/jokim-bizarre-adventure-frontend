d1b4abf3a5270e2660a6925b1b28c10080984f2d
bbang7 <zxas1245@naver.com>
2024-09-14 15:50:49 +0900

    :memo:docs: separate dummy data

--- src/components/review/ReviewSummarySection.tsx
+++ src/components/review/ReviewSummarySection.tsx
@@ -2,41 +2,7 @@ import Image from 'next/image'
 import StarAverage from '../ui/StarAverage'
 import PlusIcon from '../icons/review/PlusIcon'
 import Link from 'next/link'
-
-const reviewSummaryData: ReviewSummaryType = {
-  starAverage: 3.5,
-  evaluationItem1Name: '사이즈',
-  evaluationItem2Name: '색상',
-  evaluationItem3Name: '디자인',
-  evaluationItem1BestValue: '잘 맞아요',
-  evaluationItem2BestValue: '비슷해요',
-  evaluationItem3BestValue: '아주 만족해요',
-  evaluation1Rate: 100,
-  evaluation2Rate: 36,
-  evaluation3Rate: 55,
-  image: [
-    {
-      reviewId: 1,
-      imageId: 1,
-      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/645/202402221254645_00001.jpg?RS=300&SP=1&AO=1',
-    },
-    {
-      reviewId: 1,
-      imageId: 2,
-      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/475/202311211155475_00001.jpg?RS=300&SP=1&AO=1',
-    },
-    {
-      reviewId: 2,
-      imageId: 3,
-      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/898/202312071179898_00001.jpeg?RS=300&SP=1&AO=1',
-    },
-    {
-      reviewId: 3,
-      imageId: 4,
-      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/630/202306301013630_00001.jpeg?RS=300&SP=1&AO=1',
-    },
-  ],
-}
+import { reviewSummaryData } from '@/datas/dummy/review/ReviewData'
 
 export default function ReviewSummarySection() {
   const reviewSummary: ReviewSummaryType = reviewSummaryData
@@ -110,7 +76,7 @@ export default function ReviewSummarySection() {
                   alt="review Image"
                   layout="fill"
                   objectFit="cover"
-                  className="w-full h-full object-cover" // Tailwind로 이미지 커버 적용
+                  className="w-full h-full object-cover"
                 />
               </div>
             </li>
---
+++ src/datas/dummy/review/ReviewData.ts
@@ -0,0 +1,34 @@
+export const reviewSummaryData: ReviewSummaryType = {
+  starAverage: 3.5,
+  evaluationItem1Name: '사이즈',
+  evaluationItem2Name: '색상',
+  evaluationItem3Name: '디자인',
+  evaluationItem1BestValue: '잘 맞아요',
+  evaluationItem2BestValue: '비슷해요',
+  evaluationItem3BestValue: '아주 만족해요',
+  evaluation1Rate: 100,
+  evaluation2Rate: 36,
+  evaluation3Rate: 55,
+  image: [
+    {
+      reviewId: 1,
+      imageId: 1,
+      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/645/202402221254645_00001.jpg?RS=300&SP=1&AO=1',
+    },
+    {
+      reviewId: 1,
+      imageId: 2,
+      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/475/202311211155475_00001.jpg?RS=300&SP=1&AO=1',
+    },
+    {
+      reviewId: 2,
+      imageId: 3,
+      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/898/202312071179898_00001.jpeg?RS=300&SP=1&AO=1',
+    },
+    {
+      reviewId: 3,
+      imageId: 4,
+      imageUrl: 'https://image.sivillage.com/upload/C00001/eval/630/202306301013630_00001.jpeg?RS=300&SP=1&AO=1',
+    },
+  ],
+}
