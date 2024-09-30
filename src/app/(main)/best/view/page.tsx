ce534039915e8b130c850d775434a47b83b81e62
jangdebug <jangdebug@naver.com>
2024-09-30 10:56:00 +0900

    :sparkles:feat: Publishing & API Apply Best View Page

--- src/app/(main)/best/view/page.tsx
+++ src/app/(main)/best/view/page.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import { getBestItemAction, getBestViewItemAction } from '@/actions/best/getBestItemData'
+import ViewList from '@/components/best/ViewList'
+import { BestItemType } from '@/types/BestTypes'
 
-export default function page() {
-  return <div className="w-full h-[500px] bg-green-300">data gift</div>
+export default async function BestViewPage() {
+  const popularItemDatas: BestItemType[] = await getBestViewItemAction()
+
+  return (
+    <main>
+      <section className="my-[48px]">
+        <nav className="pt-[8px] pb-[28px] px-[24px]">
+          <p className="text-2xl font-bold">사람들이 많이 본 인기상품</p>
+        </nav>
+
+        <div className="pt-[8px] pb-[28px] px-[24px]">
+          <ViewList viewItemDatas={popularItemDatas} />
+        </div>
+      </section>
+    </main>
+  )
 }
